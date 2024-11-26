9e5851db332fc7ba7800cf823e5cd4c0320ed577
Yash goyal <yashgoyal7661@gmail.com>
2024-11-26 23:08:04 +0530

    Java

---
+++ swap.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class swap {
+    public static void main(String[] args) {
+        int a,b;
+
+        System.out.print("enter two numbers:");
+        Scanner s=new Scanner(System.in);
+        a=s.nextInt();
+        b=s.nextInt();
+        System.out.println("the number before swapping are:"+a +" " +b);
+        int temp;
+        temp=a;
+        a=b;
+        b=temp;
+        System.out.println("the number after swapping are:"+a +" " +b);
+        
+        s.close();
+
+
+    }
+    
+}
