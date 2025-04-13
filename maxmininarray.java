082fc1b27592e9710edf612e70eb66ee8e80c2ac
yashgoyal566 <yashgoyal7661@gmail.com>
2025-04-13 22:09:49 +0530

    Create maxmininarray.java

---
+++ maxmininarray.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class MinMaxInArray {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of elements in the array: ");
+        int size = scanner.nextInt();
+
+        int[] array = new int[size];
+
+        System.out.println("Enter " + size + " elements:");
+        for (int i = 0; i < size; i++) {
+            array[i] = scanner.nextInt();
+        }
+
+        int min = array[0];
+        int max = array[0];
+
+        for (int i = 1; i < size; i++) {
+            if (array[i] < min) {
+                min = array[i];
+            }
+            if (array[i] > max) {
+                max = array[i];
+            }
+        }
+
+        System.out.println("Smallest element: " + min);
+        System.out.println("Largest element: " + max);
+
+        scanner.close();
+    }
+}
